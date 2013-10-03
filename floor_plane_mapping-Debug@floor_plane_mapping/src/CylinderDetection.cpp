f54a30763c50e48848aa3903a09aaa3ad538d956
Masuzu <anveena.menethil@gmail.com>
2013-10-03 23:44:28 +0200

    Cylinder detection code completed. Remaining : TESTS AND DEBUGGING.

--- floor_plane_mapping-Debug@floor_plane_mapping/src/CylinderDetection.cpp
+++ floor_plane_mapping-Debug@floor_plane_mapping/src/CylinderDetection.cpp
@@ -14,9 +14,81 @@
 #include <map>
 #include <vector>
 
-#define THRESHOLD_TO_PARSE_PLANE	10
-#define THRESHOLD_TO_ACCEPT_CIRCLE_FOUND	10
+static unsigned int THRESHOLD_LOWER_TO_PARSE_PLANE = 10;
+static unsigned int THRESHOLD_UPPER_TO_PARSE_PLANE = 600;
+static unsigned int THRESHOLD_TO_ACCEPT_CIRCLE_FOUND = 10;
 
+//==================//
+// Helper functions //
+//==================//
+
+// Get a random index for the cloud point
+static __forceinline size_t GetRandomIndex(unsigned long i)
+{
+	return std::min((rand() / (double) RAND_MAX) * i, (double) i - 1);
+}
+
+// Calculate the distance between 2 points
+static __forceinline double GetDistanceBetweenPoints(const pcl::PointXYZ& A, const pcl::PointXYZ& B)
+{
+	return sqrt((A.x-B.x)*(A.x-B.x)+(A.y-B.y)*(A.y-B.y)+(A.z-B.z)*(A.z-B.z));
+}
+
+// Calculate the distance between the point and the plane
+static double GetDistanceToPlane(const pcl::PointXYZ& point, const Eigen::Vector3f& normalVector, double d)
+{
+	Eigen::Vector3f t;
+	t << point.x, point.y, point.z;
+	return fabs(t.dot(normalVector) + d);
+}
+
+static __forceinline bool IsInCircle(const pcl::PointXYZ& center, const double &dRadius, const pcl::PointXYZ &ptToTest, double dTolerance)
+{
+	return ( fabs(sqrt( (ptToTest.x-center.x)*(ptToTest.x-center.x) + (ptToTest.y-center.y)*(ptToTest.y-center.y) + (ptToTest.z-center.z)*(ptToTest.z-center.z) ) - dRadius) < dTolerance);
+}
+
+// Output : center and dRadius
+static void GetCenterRadiusFrom3Points2D(pcl::PointXYZ& center, double &dRadius,
+								  const pcl::PointXYZ& A, const pcl::PointXYZ& B, const pcl::PointXYZ& C)
+{
+	// Takes the bisections of AB and AC and computes their intersection
+	Eigen::Vector2f AB;
+	Eigen::Vector2f AC;
+	AB << (B.x - A.x), (B.y - A.y);
+	AC << (C.x - A.x), (C.y - A.y);
+	Eigen::Vector2f I1, I2;
+	I1 << (B.x + A.x)/2, (B.y + A.y)/2;
+	I2 << (C.x + A.x)/2, (C.y + A.y)/2;
+
+	// a1*(y-y_i1)-b1*(x-x_i1)=0 where i1 is the middle of AB
+	// a2*(y-y_i2)-b2*(x-x_i2)=0 where i2 is the middle of AC
+	double a1 = -AB.y;
+	double b1 = AB.x;
+	double a2 = -AC.y;
+	double b2 = AC.x;
+	center.z = (A.z+B.z+C.z)/3;
+	if(a1 == 0)
+	{
+		center.x = I1.x;
+		center.y = b2*(center.x-I2.x)/a2+I2.y;	// We assume AB and AC are not //. Hence a1 and a2 can't be both null.
+	}
+	else
+	{
+		center.x = (-b2*I2.x+a2*I1.x/a1+a2*(I2.y-I1.y))/(a2*b1/a1-b2);
+		center.y = b1*(center.x-I1.x)/a1+I1.y;
+	}
+	dRadius = (sqrt( (A.x-center.x)*(A.x-center.x) + (A.y-center.y)*(A.y-center.y) + (A.z-center.z)*(A.z-center.z) )
+		+ sqrt( (B.x-center.x)*(B.x-center.x) + (B.y-center.y)*(B.y-center.y) + (B.z-center.z)*(B.z-center.z) )
+		+sqrt( (C.x-center.x)*(C.x-center.x) + (C.y-center.y)*(C.y-center.y) + (C.z-center.z)*(C.z-center.z) ))/3;
+}
+
+// ^__^
+// We are done with the helper functions
+
+// CylinderDetection : THE class which does all the cylinder recognition job .
+// Since it is supposed to be used internally only for the node created in this file, we did not bother
+// declaring a header for this class.
+//===================================
 class CylinderDetection
 {
 protected:
@@ -38,8 +110,8 @@ protected:
 	double m_dDiscretizationPrecision;
 
 protected:
-	// ROS Callbacks
-
+	//==============//
+	// ROS Callback //
 	void pc_callback(const sensor_msgs::PointCloud2ConstPtr msg) {
 		pcl::PointCloud<pcl::PointXYZ> temp;
 		pcl::fromROSMsg(*msg, temp);
@@ -97,7 +169,9 @@ protected:
 
 			auto pvPoints = &(it->second);	// pvPoint has type std::vector<size_t>*
 			n = pvPoints->size();	// # points in the section 'z = it->first'
-			if(n < THRESHOLD_TO_PARSE_PLANE)
+			if(n < THRESHOLD_LOWER_TO_PARSE_PLANE)
+				continue;
+			if(n > THRESHOLD_UPPER_TO_PARSE_PLANE)
 				continue;
 
 			ROS_INFO("%d useful points in the section at z = %.2f", n, it->first);
@@ -171,14 +245,54 @@ protected:
 			ROS_INFO("Circle found at z = %.2f Score %d Center (%.2f, %.2f, %.2f) Radius %.2f", it->first, (int)best, center.x, center.y, center.z, radius );
 		}
 
+		//==================================================
 		// CONCAT THE RESULTS FROM mapCirclesInPlaneSections
-		if(it.size < 3)
+		if(mapCirclesInPlaneSections.size() < 3)
 			return;	// Not enough acceptable sections to consider that there is a cylinder
+
+		pcl::PointXYZ meanCenter = pcl::PointXYZ(0, 0, 0);
+		double meanRadius = 0;
+		int numSections = 1;
+		double dBottomCap = DBL_MAX;
+		double dTopCap;
 		for(auto it = mapCirclesInPlaneSections.begin(); it!=mapCirclesInPlaneSections.end(); ++it)
 		{
-			// TODO
+			auto itNext = it+1;
+			if(itNext != mapCirclesInPlaneSections.end())
+			{
+				if(GetDistanceBetweenPoints(it->second.first, itNext->second.first) < 0.05 && fabs(it->second.second-itNext->second.second) < 0.05)
+				{
+					if(dBottomCap == DBL_MAX)
+						dBottomCap = it->first;
+					++numSections;
+					meanCenter.x += ((itNext->second.first.x + it->second.first.x)/2.0);
+					meanCenter.y += ((itNext->second.first.y + it->second.first.y)/2.0);
+					meanCenter.z += ((itNext->second.first.z + it->second.first.z)/2.0);
+					meanRadius += ((itNext->second.second+it->second.second)/2.0);
+				}
+				else
+				{
+					dTopCap = it->first;
+					meanCenter.x /= numSections;
+					meanCenter.y /= numSections;
+					meanCenter.z /= numSections;
+					meanRadius /= numSections;
+					if(numSections >= 3)
+						// if(numSections < 3) Not enough acceptable sections to consider that there is a cylinder
+					else
+					{
+						ROS_INFO("Cylinder found: Center (%.2f, %.2f, %.2f) Radius %.2f Bottom z = %.2f Top z = %.2f", meanCenter.x, meanCenter.y, meanCenter.z, dBottomCap, dTopCap);
+					}
+					meanCenter = pcl::PointXYZ(0, 0, 0);
+					meanRadius = 0;
+					numSections = 1;
+					dBottomCap = DBL_MAX;
+				}
+			}
+
 			// ^___^
 		}
+		
 	}
 
 public:
@@ -188,10 +302,12 @@ public:
 			m_NodeHandle.param("max_range", m_dMaxRange, 5.0);
 			m_NodeHandle.param("n_samples", m_nSamples, 1000);
 			m_NodeHandle.param("tolerance", m_dTolerance, 1.0);
+			m_NodeHandle.param("parse_plane_lower_bound_threshhold", THRESHOLD_LOWER_TO_PARSE_PLANE, 10);
+			m_NodeHandle.param("parse_plane_upper_bound_threshhold", THRESHOLD_UPPER_TO_PARSE_PLANE, 600);
+			m_NodeHandle.param("accept_cylinder_threshhold", THRESHOLD_TO_ACCEPT_CIRCLE_FOUND, 10);
 
 			ROS_INFO("Searching for vertical cylinders");
-			ROS_INFO(
-				"RANSAC: %d iteration with %f tolerance", m_nSamples, tolerance);
+			ROS_INFO("RANSAC: %d iteration with %f tolerance", m_nSamples, tolerance);
 			assert(m_nSamples > 0);
 
 			// Make sure TF is ready
@@ -204,20 +320,6 @@ public:
 
 	}
 
-	// Get a random index for the cloud point
-	__forceinline size_t GetRandomIndex(unsigned long i)
-	{
-		return std::min((rand() / (double) RAND_MAX) * i, (double) i - 1);
-	}
-
-	// Calculate the distance between the point and the plane
-	double GetDistanceToPlane(pcl::PointXYZ& point, Eigen::Vector3f& normalVector,
-		double d) {
-			Eigen::Vector3f t;
-			t << point.x, point.y, point.z;
-			return fabs(t.dot(normalVector) + d);
-	}
-
 	// Get the elapsed time from start to end
 	long GetElapsedTime(timeval& start, timeval& end)
 	{
@@ -227,51 +329,18 @@ public:
 		mtime = ((seconds) * 1000 + useconds / 1000.0) + 0.5;
 		return mtime;
 	}
-
-	__forceinline bool IsInCircle(const pcl::PointXYZ& center, const double &dRadius, const pcl::PointXYZ &ptToTest, double dTolerance)
-	{
-		return ( fabs(sqrt( (ptToTest.x-center.x)*(ptToTest.x-center.x) + (ptToTest.y-center.y)*(ptToTest.y-center.y) + (ptToTest.z-center.z)*(ptToTest.z-center.z) ) - dRadius) < dTolerance);
-	}
-
-	void GetCenterRadiusFrom3Points2D(pcl::PointXYZ& center, double &dRadius,
-		const pcl::PointXYZ& A, const pcl::PointXYZ& B, const pcl::PointXYZ& C)
-	{
-		// Takes the bisections of AB and AC and computes their intersection
-		Eigen::Vector2f AB;
-		Eigen::Vector2f AC;
-		AB << (B.x - A.x), (B.y - A.y);
-		AC << (C.x - A.x), (C.y - A.y);
-		Eigen::Vector2f I1, I2;
-		I1 << (B.x + A.x)/2, (B.y + A.y)/2;
-		I2 << (C.x + A.x)/2, (C.y + A.y)/2;
-
-		// a1*(y-y_i1)-b1*(x-x_i1)=0 where i1 is the middle of AB
-		// a2*(y-y_i2)-b2*(x-x_i2)=0 where i2 is the middle of AC
-		double a1 = -AB.y;
-		double b1 = AB.x;
-		double a2 = -AC.y;
-		double b2 = AC.x;
-		center.z = (A.z+B.z+C.z)/3;
-		if(a1 == 0)
-		{
-			center.x = I1.x;
-			center.y = b2*(center.x-I2.x)/a2+I2.y;	// We assume AB and AC are not //. Hence a1 and a2 can't be both null.
-		}
-		else
-		{
-			center.x = (-b2*I2.x+a2*I1.x/a1+a2*(I2.y-I1.y))/(a2*b1/a1-b2);
-			center.y = b1*(center.x-I1.x)/a1+I1.y;
-		}
-		dRadius = (sqrt( (A.x-center.x)*(A.x-center.x) + (A.y-center.y)*(A.y-center.y) + (A.z-center.z)*(A.z-center.z) )
-			+ sqrt( (B.x-center.x)*(B.x-center.x) + (B.y-center.y)*(B.y-center.y) + (B.z-center.z)*(B.z-center.z) )
-			+sqrt( (C.x-center.x)*(C.x-center.x) + (C.y-center.y)*(C.y-center.y) + (C.z-center.z)*(C.z-center.z) ))/3;
-	}
 };
+
+///////////////////////
+// M   M  AA  I N  N //
+// MM MM A  A I NN N //
+// M M M AAAA I N NN //
+// M   M A  A I N  N //
+///////////////////////
 
 int main(int argc, char * argv[]) {
 	ros::init(argc, argv, "CylinderDetection");
-	CylinderDetection fp;
-
+	CylinderDetection node;
 	ros::spin();
 	return 0;
 }
